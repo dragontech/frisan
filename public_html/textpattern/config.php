8ed4365fe5689f10df988931e7e472b979e6a101
Mihai Frisan <mihai.frisan@sparktech.ro>
2011-05-12 18:18:59 +0300

    initial commit

---
+++ public_html/textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'mfrisan_mihai';
+$txpcfg['user'] = 'mfrisan_mihai';
+$txpcfg['pass'] = '2WTxE>Mcv3*6';
+$txpcfg['host'] = 'localhost';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = '/home/mfrisan/public_html/mihai/textpattern';
+$txpcfg['dbcharset'] = 'utf8';
+?>
